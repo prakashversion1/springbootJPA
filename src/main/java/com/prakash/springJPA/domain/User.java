49a9a17c57b4a5d5a3fe9d48e213b676192fdd9c
Lalit Joshi <lalualpha123@gmail.com>
2017-12-31 07:30:34 +0545

    add User.java class

---
+++ src/main/java/com/prakash/springJPA/domain/User.java
@@ -0,0 +1,38 @@
+package com.prakash.springJPA.domain;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "created")
+    @CreationTimestamp
+    private Date created;
+
+    @Column(name = "updated")
+    @UpdateTimestamp
+    private Date updated;
+
+    //getters and setters
+
+}
